d1c43008b8a52911b02adaa5e51d1d81d0f7080f
tptam <tptam@LAPTOP-Q3BGEPVH>
2022-02-09 13:11:17 +0900

    Fixed bug in adjustCoordsToCanvas (y was reciprocal of the true value)

--- java_learning/src/SpiderChart.java
+++ java_learning/src/SpiderChart.java
@@ -14,8 +14,6 @@ import javafx.geometry.VPos;
  * 
  */
 public class SpiderChart extends Application{
-	final int WIDTH = 500;
-	final int HEIGHT = 500;
 	
 	public double[] polarToXY(double r, double theta) {
 		double[] xy = new double[2];
@@ -25,17 +23,22 @@ public class SpiderChart extends Application{
 	}
 	
 	public double ithTheta(int n, int i) {
-		return Math.PI * 1.5 + i * 2 * Math.PI / n;
+		double theta0 = 0.5 * Math.PI;
+		double thetaDiff = i * (2 * Math.PI / n);
+		return theta0 - thetaDiff;
 	}
 	
 	public double[] adjustCoordsToCanvas(double x, double y, int width, int height) {
 		double[] xyCanvas = new double[2];
 		xyCanvas[0] = width / 2 + x;
-		xyCanvas[1] = height / 2 + y;
+		xyCanvas[1] = height / 2 - y;
 		return xyCanvas;
 	}
 	
 	public void drawChart(GraphicsContext g, int width, int height, double[] data) {
+		double webStep = width / 10;
+		double webR = 4 * webStep;
+
 		// fill background
 		g.setFill(Color.WHITE);
 		g.fillRect(0, 0, width, height);
@@ -45,23 +48,28 @@ public class SpiderChart extends Application{
 		double[] x = new double[n];
 		double[] y = new double[n];
 		for (int i = 0; i < n; i++) {
-			double[] xy = polarToXY(200 * data[i], ithTheta(n, i));
-			x[i] = xy[0] + 250;
-			y[i] = xy[1] + 250;
+			double[] xy = polarToXY(webR * data[i], ithTheta(n, i));
+			xy = adjustCoordsToCanvas(xy[0], xy[1], width, height);
+			x[i] = xy[0];
+			y[i] = xy[1];
 		}
 		g.setFill(Color.LIGHTBLUE);
 		g.fillPolygon(x, y, n);
 		
 		// draw circles
-		g.strokeOval(50, 50, 400, 400);
-		g.strokeOval(100, 100, 300, 300);
-		g.strokeOval(150, 150, 200, 200);
-		g.strokeOval(200, 200, 100, 100);
 		
+		for (int i = 1; i < 5; i++) {
+			double start = webStep * i;
+			double end = width - webStep * 2 * i;
+			System.out.println(start + " " + end);
+			g.strokeOval(start, start, end, end);
+		}
+
 		// draw axis
 		for (int i = 0; i < n; i++) {
-			double[] xy = polarToXY(200, ithTheta(n, i));
-			g.strokeLine(250, 250, xy[0]+250, xy[1]+250);
+			double[] xy = polarToXY(webR, ithTheta(n, i));
+			xy = adjustCoordsToCanvas(xy[0], xy[1], width, height);
+			g.strokeLine(width/2, height/2, xy[0], xy[1]);
 		}
 
 
@@ -69,8 +77,9 @@ public class SpiderChart extends Application{
 		g.setTextAlign(TextAlignment.CENTER);
 		g.setTextBaseline(VPos.CENTER);
 		for (int i = 0; i < n; i++) {
-			double[] xy = polarToXY(220, ithTheta(n, i));
-			g.strokeText(String.valueOf(data[i]), xy[0]+250, xy[1]+250);
+			double[] xy = polarToXY(width/2 - 30, ithTheta(n, i));
+			xy = adjustCoordsToCanvas(xy[0], xy[1], width, height);
+			g.strokeText(String.valueOf(data[i]), xy[0], xy[1]);
 		}
 	}
 	
@@ -79,10 +88,11 @@ public class SpiderChart extends Application{
 	}
 	
 	public void start(Stage stage) {		
-		int width = WIDTH;
-		int height = HEIGHT;
+		int width = 500;
+		int height = width;
 		Canvas canvas = new Canvas(width, height);
 		GraphicsContext g = canvas.getGraphicsContext2D();
+		
 		// dummy data
 		double[] data = {0.1, 0.2, 0.3, 0.4, 0.5, 0.6};
 		drawChart(g, width, height, data);
